ad1b505d1dcc75ae346adb4fb7a4be46673c87d6
Gautham Srinivas <gsri0828@uni.sydney.edu.au>
2023-07-25 13:19:49 +0700

    added sounds

--- studio/src/App.jsx
+++ studio/src/App.jsx
@@ -1,8 +1,17 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import Spline from "@splinetool/react-spline";
+import soundFile from "./assets/sdo.mp3";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeUp } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const audio = new Audio(soundFile);
+
+  function playSound() {
+    audio.play();
+  }
+
   useEffect(() => {
     var modal = document.getElementById("myModal");
     var span = document.getElementById("close");
@@ -35,10 +44,15 @@ function App() {
       </div>
       <div id="content">
         <div id="spline">
-        <Spline scene="https://prod.spline.design/zDTdTtLm0GVaN2GR/scene.splinecode" />
+          <Spline scene="https://prod.spline.design/zDTdTtLm0GVaN2GR/scene.splinecode" />
         </div>
         <div id="text">
-          <h1 id="heading">Angklung</h1>
+          <div className="headingContainer">
+            <h1 id="heading">Angklung</h1>
+            <button onClick={playSound} className="soundButton">
+              <FontAwesomeIcon icon={faVolumeUp} />
+            </button>
+          </div>
           <p>
             Angklung adalah alat musik bambu yang telah ada sejak berabad-abad
             lalu dan merupakan bagian penting dari budaya Sunda. Alat musik ini
